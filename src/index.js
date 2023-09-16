14dfddd0031c1685ae4ac707e7ca8cdeb9ea392a
Hritvik Mohan <hritvik5005@gmail.com>
2023-09-17 01:22:18 +0530

    Updated

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const resources = JSON.parse(fs.readFileSync(`${__dirname}/data/resources.json`)
 
 dotenv.config();
 const app = express();
+
+const server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
